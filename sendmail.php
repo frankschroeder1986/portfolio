b4d3a328be53bd9030dd292f759298bb200c2e7b
frankschroeder1986 <113301313+frankschroeder1986@users.noreply.github.com>
2023-04-02 11:02:13 +0200

    w

--- sendmail.php
+++ sendmail.php
@@ -1,25 +1,11 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    // Empfänger E-Mail-Adresse
-    $empfaenger = "frankschroeder1986@gmail.com";
+$emp = "frankschroeder1986@gmail.com";
+$betreff = "neu";
+$from = "From: Frank <frank.schroeder24@gmx.de>\n";
+$from .= "Reply-To: frankschroeder1986@gmail.com\n";
+$from .= "Content-Type: text/html\n";
+$text = "<h1> HTMLWORLDS huhu</h1>";
 
-    // Betreff der E-Mail
-    $betreff = "Neue Nachricht von " . $_POST["name"];
 
-    // Nachrichtentext
-    $nachricht = "Name: " . $_POST["name"] . "\n";
-    $nachricht .= "E-Mail: " . $_POST["email"] . "\n\n";
-    $nachricht .= $_POST["message"];
-
-    // E-Mail-Header
-    $header = "From: " . $_POST["email"] . "\r\n";
-    $header .= "Reply-To: " . $_POST["email"] . "\r\n";
-
-    // E-Mail senden
-    mail($empfaenger, $betreff, $nachricht, $header);
-
-    // Bestätigungsmeldung ausgeben
-    echo "Vielen Dank für Ihre Nachricht!";
-}
-?>
+mail($emp, $betreff, $text, $from);
