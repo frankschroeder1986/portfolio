4b3835ee9abf457fc1c2849a819a0b299078e402
frankschroeder1986 <113301313+frankschroeder1986@users.noreply.github.com>
2023-04-02 10:28:17 +0200

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,62 +1,25 @@
 <?php
-if(isset($_POST['email'])) {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $email_to = "frankschroeder1986@gmail.com";
-    $email_subject = "Kontaktformular: ".$_POST['name'];
+    // Empfänger E-Mail-Adresse
+    $empfaenger = "frankschroeder1986@gmail.com";
 
-    function died($error) {
-        echo "Es tut uns leid, aber es gibt Fehler im Formular. ";
-        echo "Diese Fehler erscheinen unten.<br /><br />";
-        echo $error."<br /><br />";
-        echo "Bitte beheben Sie diese Fehler.<br /><br />";
-        die();
-    }
+    // Betreff der E-Mail
+    $betreff = "Neue Nachricht von " . $_POST["name"];
 
-    // validation expected data exists
-    if(!isset($_POST['name']) ||
-        !isset($_POST['email']) ||
-        !isset($_POST['message'])) {
-        died('Es tut uns leid, aber es gibt Fehler im Formular.');       
-    }
+    // Nachrichtentext
+    $nachricht = "Name: " . $_POST["name"] . "\n";
+    $nachricht .= "E-Mail: " . $_POST["email"] . "\n\n";
+    $nachricht .= $_POST["message"];
 
-    $name = $_POST['name']; // required
-    $email_from = $_POST['email']; // required
-    $message = $_POST['message']; // required
+    // E-Mail-Header
+    $header = "From: " . $_POST["email"] . "\r\n";
+    $header .= "Reply-To: " . $_POST["email"] . "\r\n";
 
-    $error_message = "";
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/';
+    // E-Mail senden
+    mail($empfaenger, $betreff, $nachricht, $header);
 
-    if(!preg_match($email_exp,$email_from)) {
-        $error_message .= 'Die eingegebene E-Mail-Adresse ist ungültig.<br />';
-    }
-
-    $string_exp = "/^[A-Za-z .'-]+$/";
-
-    if(!preg_match($string_exp,$name)) {
-        $error_message .= 'Der eingegebene Name ist ungültig.<br />';
-    }
-
-    if(strlen($message) < 2) {
-        $error_message .= 'Die eingegebene Nachricht ist ungültig.<br />';
-    }
-
-    if(strlen($error_message) > 0) {
-        died($error_message);
-    }
-
-    $email_message = "Formulardetails unten:\n\n";
-
-    function clean_string($string) {
-        $bad = array("content-type","bcc:","to:","cc:","href");
-        return str_replace($bad,"",$string);
-    }
-
-    $email_message .= "Name: ".clean_string($name)."\n";
-    $email_message .= "E-Mail: ".clean_string($email_from)."\n";
-    $email_message .= "Nachricht: ".clean_string($message)."\n";
-
-    // create email headers
-    $headers = 'From: '.$email_from."\r\n".
-    'Reply-To: '.$email_from."\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-    @mail($email_to
+    // Bestätigungsmeldung ausgeben
+    echo "Vielen Dank für Ihre Nachricht!";
+}
+?>
